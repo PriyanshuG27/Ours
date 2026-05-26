8cb3c4752bcce04722dbd68e25d608d050f1d3c4
Priyanshu Gumber <pri2712gumber@gmail.com>
2026-05-27 00:19:42 +0530

    ..

--- lib/supabase/admin.ts
+++ lib/supabase/admin.ts
@@ -1,13 +1,8 @@
-import { createClient as createSupabaseClient } from "@supabase/supabase-js";
-import { type Database } from "@/types/database.types";
-
-if (typeof window !== "undefined") {
-  throw new Error(
-    "Supabase admin client cannot be imported on the client side.",
-  );
-}
+import 'server-only'
+import { createClient as createSupabaseClient } from '@supabase/supabase-js'
+import { type Database } from '@/types/database.types'
 
 export const adminClient = createSupabaseClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!,
-);
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+)
